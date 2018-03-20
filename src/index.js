629e7dee16918599f497ffddfe48bbd294f1b7ed
hzlshen <m15277968105@163.com>
2018-03-20 15:07:17 +0800

    JSX

--- src/index.js
+++ src/index.js
@@ -1,74 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+//JSX， 一种 JavaScript 的语法扩展
+function formatName(user) {
+    return user.firstName + '' +user.lastName;
+}
+
+const user ={
+    firstName : '神威如狱',
+    lastName : '不动的推动者'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}
+    </h1>
+);
 
 ReactDOM.render(
-    <App/>, document.getElementById('root'));
-registerServiceWorker();
-
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {this.props.value}
-        </button>
-      );
-    }
-  }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i} />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
-  
+    element,document.getElementById('root')
+)
